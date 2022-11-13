d18726a0819672206c048369d1537a8f77d51fcc
Webbdevs <116191301+Webbdevs@users.noreply.github.com>
2022-11-13 22:35:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,17 +6,12 @@ function searchIt(){
 	let searchTxt = document.getElementById('search-box').value
 	let mobName = document.querySelectorAll('.names')
 	let element;
-	/*if(searchTxt=="Iphone" || searchTxt=="Pixel")
-	{
-		document.getElementById('iphone-pixel-compare').style.backgroundColor = "yellow"
-		//window.location.href = "iphone-pixel.html"
-	}*/
 	for(element of mobName)
 	{
 		if(searchTxt==element.innerText)
 		{
+			element.scrollIntoView()
 			element.parentElement.style.backgroundColor = "limegreen"
-	//	console.log(searchTxt)
 		}
 	}
 }
