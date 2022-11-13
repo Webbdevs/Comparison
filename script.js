44f4b295ab3e13e40f3e35bfdd127f93a6b4e793
Webbdevs <116191301+Webbdevs@users.noreply.github.com>
2022-11-13 18:57:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,22 @@ let ipContainer = document.getElementById('iphone-pixel-compare')
 ipContainer.onclick = () => {
 	window.location.href = "iphone-pixel.html";
 }
+function searchIt(){
+	let searchTxt = document.getElementById('search-box').value
+	let mobName = document.querySelectorAll('.names')
+	let element;
+	/*if(searchTxt=="Iphone" || searchTxt=="Pixel")
+	{
+		document.getElementById('iphone-pixel-compare').style.backgroundColor = "yellow"
+		//window.location.href = "iphone-pixel.html"
+	}*/
+	for(element of mobName)
+	{
+		if(searchTxt==element.innerText)
+		{
+			element.parentElement.style.backgroundColor = "limegreen"
+	//	console.log(searchTxt)
+		}
+	}
+}
+	
