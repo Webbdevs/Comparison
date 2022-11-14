06600d55a7584d4961f6bb1b0189e384d3be0d90
Webbdevs <116191301+Webbdevs@users.noreply.github.com>
2022-11-14 19:02:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,26 +2,33 @@ let ipContainer = document.getElementById('iphone-pixel-compare')
 ipContainer.onclick = () => {
 	window.location.href = "iphone-pixel.html";
 }
+
+let searchTxt = document.getElementById('search-box').value
+//let arr = []
+
 function searchIt(){
 	let searchTxt = document.getElementById('search-box').value
 	let mobName = document.querySelectorAll('.names')
 	let element;
-	let sLeft;
-	let sTop;
-	/*if(searchTxt=="Iphone" || searchTxt=="Pixel")
-	{
-		document.getElementById('iphone-pixel-compare').style.backgroundColor = "yellow"
-		//window.location.href = "iphone-pixel.html"
-	}*/
+	let counter = 0;
+
 	for(element of mobName)
 	{
-		if(searchTxt==element.innerText)
+		let arr = element.innerText.split('')
+		//arr.push(element.innerHTML)
+		//console.log(arr)
+		for(let i = 0; i < arr.length; i++)
 		{
+			//console.log(element)
+		//	counter++;
+			//console.log(arr[i])
+				//		console.log(arr[i][counter])
+			if(arr[i]==searchTxt)
+			{			
 			element.scrollIntoView()
 			element.parentElement.style.backgroundColor = "limegreen"
-			sTop = element.scrollTop
+			console.log(arr[i])
+			}
 		}
-	}
+	}	
 }
-		//	alert(sTop)
-	
